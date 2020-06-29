8d097ded060bf1abbc8ba42a10dabf0aa296677d
sebTerje <sebastianmennen@gmail.com>
2020-06-29 15:57:56 +0200

    added address and contact class

--- ContactDBLibrary/Class1.cs
+++ ContactDBLibrary/Class1.cs
@@ -5,9 +5,39 @@ namespace ContactDBLibrary
 {
     public class Contact
     {
-        public int Id { get; set; }
-        public int SSN { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public int Id { get; private set; }
+        public int SSN { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Contact(int id, int sSN, string firstName, string lastName)
+        {
+            Id = id;
+            SSN = sSN;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+    }
+
+    public class Address
+    {
+        public int Id { get; private set; }
+        public string Street { get; private set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+
+
+        public Address(int id, string street, string city, int zip)
+        {
+            Id = id;
+            Street = street;
+            City = city;
+            Zip = zip;
+        }
+
+        
+
+
     }
 }
