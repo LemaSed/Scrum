2ed44ef205855b5273e95fbcd247958a78ae50b0
sebTerje <sebastianmennen@gmail.com>
2020-06-29 15:45:09 +0200

    Added ContactDBLib

---
+++ ContactDBLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ContactDBLibrary
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public int SSN { get; set; }
+        public string FirstName { get; set; }
+    }
+}
