73e8950f3490108ad67aac0ceff33e9e03823d86
sebTerje <sebastianmennen@gmail.com>
2020-06-29 15:47:54 +0200

    test

--- ContactDBLibrary/Class1.cs
+++ ContactDBLibrary/Class1.cs
@@ -8,5 +8,6 @@ namespace ContactDBLibrary
         public int Id { get; set; }
         public int SSN { get; set; }
         public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
